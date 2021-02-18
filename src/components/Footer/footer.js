af576a796d52e123a25c263c56d9ca69001de9a1
Lee Stratman <stratmanlc10@gmail.com>
2021-02-17 19:30:29 -0600

    Add footer component. Fix class to className in app component.

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = (props) => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div>
+        <span className="pl-3 text-white">Developed for devCodeCamp</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import Footer from "./Footer/footer";
 
 class App extends React.Component {
   render() {
     return (
-      <div class="p-4 p-md-5 mb-4 text-white bg-dark">
-        <div class="col-md-6 px-0">
-          <h1 class="display-4 fst-italic">Music Library</h1>
-          <p class="lead my-3">For your listening pleasure.</p>
+      <div>
+        <div className="p-4 p-md-5 mb-4 text-white bg-dark">
+          <div className="col-md-6 px-0">
+            <h1 className="display-4 fst-italic">Music Library</h1>
+            <p className="lead my-3">For your listening pleasure.</p>
+          </div>
         </div>
+        <Footer />
       </div>
     );
   }
