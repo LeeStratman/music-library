370f45c658d03324c275fd2fb2588b73d5f1e4e8
Lee Stratman <stratmanlc10@gmail.com>
2021-02-17 19:20:59 -0600

    Add bootstrap. Add app component.

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="p-4 p-md-5 mb-4 text-white bg-dark">
+        <div class="col-md-6 px-0">
+          <h1 class="display-4 fst-italic">Music Library</h1>
+          <p class="lead my-3">For your listening pleasure.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
