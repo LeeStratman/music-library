cc03d4bb1d128e20f617ee243170bf99f2176759
Lee Stratman <stratmanlc10@gmail.com>
2021-02-23 18:59:18 -0600

    Update header component to use Tailwind.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-const Header = (props) => {
+const Header = () => {
   return (
-    <header className="p-4 p-md-5 mb-4 text-white bg-dark">
-      <div className="col-md-6 px-0">
-        <h1 className="display-4 fst-italic">Music Library</h1>
-        <p className="lead my-3">For your listening pleasure.</p>
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">
+            Personalized
+          </h2>
+          <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+            Music Library
+          </p>
+          <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+            For your listening pleasure.
+          </p>
+        </div>
       </div>
-    </header>
+    </div>
   );
 };
 
