5a531cac5b22e7308af3cbbcc1caf754b9f8ef13
Lee Stratman <stratmanlc10@gmail.com>
2021-02-26 12:53:40 -0600

    Refactor PlaylistSidebar component.  Add AddPlaylistForm component.

---
+++ src/components/AddPlaylistForm/addPlaylistForm.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { playlistExists } from "../../utils/playlists";
+
+const AddPlaylistForm = ({ playlists, addPlaylist }) => {
+  const [playlistName, setPlaylistName] = useState("");
+  const [error, setError] = useState(false);
+
+  const errorClass = error
+    ? "focus:border-red-500 border-red-500"
+    : "focus:border-indigo-500  border-indigo-300";
+
+  const handleNameChange = (event) => {
+    const { value } = event.target;
+    setPlaylistName(value);
+    !playlistExists(playlists, value) ? setError(true) : setError(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let name = event.target.elements.addPlaylist.value;
+    addPlaylist(name);
+    setPlaylistName("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label
+        htmlFor="addPlaylist"
+        className="block text-sm font-medium text-gray-700 sr-only"
+      >
+        Add playlist
+      </label>
+      <div className="mt-1 flex rounded-md shadow-sm">
+        <div className="relative flex items-stretch flex-grow focus-within:z-10">
+          <input
+            type="text"
+            name="addPlaylist"
+            id="addPlaylist"
+            className={`focus:ring-indigo-500 block w-full rounded-none rounded-l-md pl-2 sm:text-sm ${errorClass}`}
+            placeholder="Add playlist"
+            onChange={handleNameChange}
+            value={playlistName}
+          />
+        </div>
+        <button
+          disabled={error || playlistName.trim() === ""}
+          type="submit"
+          className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+        >
+          <svg
+            className="h-5 w-5 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+          <span className="sr-only">Add</span>
+        </button>
+      </div>
+      {error && (
+        <p className="text-indigo-300 text-sm">Error: Name must be unique.</p>
+      )}
+    </form>
+  );
+};
+
+export default AddPlaylistForm;
--- src/components/PlaylistSidebar/playlistSidebar.js
+++ src/components/PlaylistSidebar/playlistSidebar.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { playlistExists } from "../../utils/playlists";
+import React from "react";
+import AddPlaylistForm from "../AddPlaylistForm/addPlaylistForm";
 
 const PlaylistSidebar = ({
   playlists,
@@ -8,29 +8,10 @@ const PlaylistSidebar = ({
   length,
   showPlaylist,
 }) => {
-  const [playlistName, setPlaylistName] = useState("");
-  const [error, setError] = useState(false);
-
-  const handleNameChange = (event) => {
-    const { value } = event.target;
-
-    setPlaylistName(value);
-    !playlistExists(playlists, value) ? setError(true) : setError(false);
-  };
-
-  const handleAddPlaylistForm = (event) => {
-    event.preventDefault();
-    let name = event.target.elements.addPlaylist.value;
-    addPlaylist(name);
-  };
-
   return (
     <div className="flex flex-col flex-shrink-0 w-64">
-      <div className="flex-1 flex flex-col pb-4 overflow-y-auto">
-        <ul
-          className="flex-1 px-2 py-5 bg-indigo-600 space-y-1 list-none"
-          aria-label="Sidebar"
-        >
+      <div className="flex-1 flex flex-col px-2 pb-4 bg-indigo-600 overflow-y-auto">
+        <ul className="py-5 space-y-1 list-none" aria-label="Sidebar">
           <li className="bg-indig-900 text-indigo group flex items-center px-2 py-2 text-sm font-medium rounded-md">
             Playlists
           </li>
@@ -66,67 +47,8 @@ const PlaylistSidebar = ({
               </button>
             </li>
           ))}
-
-          <form onSubmit={handleAddPlaylistForm}>
-            <label
-              htmlFor="addPlaylist"
-              className="block text-sm font-medium text-gray-700 sr-only"
-            >
-              Add playlist
-            </label>
-            <div className="mt-1 flex rounded-md shadow-sm">
-              <div className="relative flex items-stretch flex-grow focus-within:z-10">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <svg
-                    className="h-5 w-5 text-gray-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 10h16M4 14h16M4 18h16"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  name="addPlaylist"
-                  id="addPlaylist"
-                  className="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300"
-                  placeholder="Add playlist"
-                  onChange={handleNameChange}
-                  value={playlistName}
-                />
-                {error && <p>error</p>}
-              </div>
-              <button
-                disabled={error || playlistName.trim() === ""}
-                type="submit"
-                className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-              >
-                <svg
-                  className="h-5 w-5 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                  />
-                </svg>
-                <span className="sr-only">Add</span>
-              </button>
-            </div>
-          </form>
         </ul>
+        <AddPlaylistForm playlists={playlists} addPlaylist={addPlaylist} />
       </div>
     </div>
   );
