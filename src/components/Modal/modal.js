52d86b6eaadc2974425a65fff4fa950035bdf25c
Lee Stratman <stratmanlc10@gmail.com>
2021-02-27 15:13:32 -0600

    Move modal close button to top right of modal panel.

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -32,16 +32,31 @@ const Modal = ({ active, close, children }) => {
           aria-modal="true"
           aria-labelledby="modal-headline"
         >
-          {children}
-          <div className="mt-5 sm:mt-6">
+          <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
             <button
               onClick={() => close()}
               type="button"
-              className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+              className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Go back to dashboard
+              <span className="sr-only">Close</span>
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
             </button>
           </div>
+          {children}
         </div>
       </div>
     </div>
